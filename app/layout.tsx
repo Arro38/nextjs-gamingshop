e2d7af7a2ab3b2bec4ba2b0dd986ace220fddfd8
formation.etienne.re <formation.etienne.re@gmail.com>
2024-05-12 02:53:06 +0000

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Videodrive",
+  description: "Exercice - Videodrive - Boutique de jeux video",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="fr">
+      <body className={inter.className}>
+        <nav>
+
+        </nav>
+        {children}</body>
+    </html>
+  );
+}

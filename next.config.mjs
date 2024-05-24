50d498afb1cfa686f0c12a82f660cce1c941ea1f
arro38 <etienne.vaytilingom.pro@gmail.com>
2024-05-24 21:05:14 +0400

    chore: Update image source URL for lazy loading

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "images.jeuxvideo.com",
+        hostname: "image.jeuxvideo.com",
         port: "",
       },
     ],

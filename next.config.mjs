3a1dc00086513a746ee79c0f0924030816661eb6
arro38 <etienne.vaytilingom.pro@gmail.com>
2024-06-13 11:01:39 +0400

    production ready to coolify

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,10 @@
 /** @type {import('next').NextConfig} */
-// Add image url : https://picsum.photos
 const nextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "images.unsplash.com",
-        port: "",
-      },
-      {
-        protocol: "http",
-        hostname: "videodrive.fr",
         port: "",
       },
       {
